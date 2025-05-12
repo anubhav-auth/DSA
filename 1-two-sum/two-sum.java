aafc7b202eee5705a509c21101ee87c21f00bb74
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2025-05-13 02:36:39 +0530

    Time: 2 ms (98.81%) | Memory: 44.8 MB (77.84%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -5,10 +5,9 @@ class Solution {
         for(int i = 0; i< nums.length; i++){
             if(h.containsKey(target-nums[i])){
                 return new int[] {h.get(target-nums[i]), i};
-            }else{
-                h.put(nums[i], i);
             }
+            h.put(nums[i], i);
         }
-        return new int[] {};
+        return null;
     }
 }
