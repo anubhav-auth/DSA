39547ee6baa207b432a75ef8aa97d102c59f4239
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2025-05-14 02:19:15 +0530

    Time: 2 ms (88.54%) | Memory: 55.9 MB (29.51%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int l = nums.length; 
-        int[] prefix = new int[l];
-        int[] output = new int[l];
+        int[] prefix = new int[nums.length];
+        int[] output = new int[nums.length];
 
         prefix[0] = 1;
 
-        for(int i = 1; i<l; i++){
+        for(int i = 1; i<nums.length; i++){
             prefix[i] = nums[i-1] * prefix[i-1];
         }
 
         int suffixProd = 1;
 
-        for(int i = l-1; i >= 0; i--){
+        for(int i = nums.length-1; i >= 0; i--){
             output[i] = prefix[i] * suffixProd;
             suffixProd *= nums[i];
         }
