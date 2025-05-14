38acc16e6fe706bd7aa444c03fc4f4e5b5920d9c
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2025-05-15 01:32:39 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (33.54%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,41 +1,46 @@
-class Solution {
+public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
-        
-        for (int i = 0; i < 9; i++) {
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
-        }
-        
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char current = board[i][j];
-                
-                if (current == '.') {
-                    continue;
+
+        for (int row = 0; row < 9; row++) {
+            boolean[] seen = new boolean[9];
+            for (int col = 0; col < 9; col++) {
+                char current = board[row][col];
+                if (current != '.') {
+                    int num = current - '1'; 
+                    if (seen[num]) return false;
+                    seen[num] = true;
                 }
-                
-                if (rows[i].contains(current)) {
-                    return false;
-                }
-                rows[i].add(current);
-                
-                if (cols[j].contains(current)) {
-                    return false;
-                }
-                cols[j].add(current);
-                
-                int boxIndex = (i / 3) * 3 + j / 3;
-                if (boxes[boxIndex].contains(current)) {
-                    return false;
-                }
-                boxes[boxIndex].add(current);
             }
         }
-        
+
+        for (int col = 0; col < 9; col++) {
+            boolean[] seen = new boolean[9];
+            for (int row = 0; row < 9; row++) {
+                char current = board[row][col];
+                if (current != '.') {
+                    int num = current - '1';
+                    if (seen[num]) return false;
+                    seen[num] = true;
+                }
+            }
+        }
+
+        for (int boxRow = 0; boxRow < 9; boxRow += 3) {
+            for (int boxCol = 0; boxCol < 9; boxCol += 3) {
+                boolean[] seen = new boolean[9];
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        char current = board[boxRow + i][boxCol + j];
+                        if (current != '.') {
+                            int num = current - '1';
+                            if (seen[num]) return false;
+                            seen[num] = true;
+                        }
+                    }
+                }
+            }
+        }
+
         return true;
     }
 }
