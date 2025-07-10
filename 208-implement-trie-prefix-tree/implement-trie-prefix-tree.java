255ebbf236d9ace5255b3b7b54e54905ddaf4181
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2025-07-10 18:25:15 +0530

    Time: 34 ms (71.20%) | Memory: 55.1 MB (92.98%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,40 +1,48 @@
-class TrieNode{
-    HashMap<Character, TrieNode> children = new HashMap<>();
-    Boolean isEnd = false;
-}
 
 class Trie {
+
+    private static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        Boolean isEnd = false;
+    }
+
     TrieNode root;
 
     public Trie() {
         root = new TrieNode();
     }
-    
+
     public void insert(String word) {
         TrieNode node = root;
 
-        for(char ch: word.toCharArray()){
-            node = node.children.computeIfAbsent(ch, c -> new TrieNode());
+        for (char ch : word.toCharArray()) {
+            int i = ch - 'a';
+            if (node.children[i] == null)
+                node.children[i] = new TrieNode();
+            node = node.children[i];
         }
 
         node.isEnd = true;
     }
-    
+
     public boolean search(String word) {
         TrieNode node = getNode(word);
         return node != null && node.isEnd;
     }
-    
+
     public boolean startsWith(String prefix) {
         return getNode(prefix) != null;
     }
 
-    public TrieNode getNode(String str){
+    public TrieNode getNode(String str) {
         TrieNode node = root;
 
-        for(char ch: str.toCharArray()){
-            node = node.children.get(ch);
-            if(node == null) return null;
+        for (char ch : str.toCharArray()) {
+            int i = ch - 'a';
+            
+            if (node.children[i] == null)
+                return null;
+            node = node.children[i];
         }
 
         return node;
