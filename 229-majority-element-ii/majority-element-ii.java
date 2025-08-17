1be9213902eb6033dc23312683ec1d1829505852
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2025-08-17 16:47:37 +0530

    Time: 14 ms (40.90%) | Memory: 50.1 MB (81.10%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,38 +1,17 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Integer candidate1 = null, candidate2 = null;
-        int count1 = 0, count2 = 0;
-
-        // 1st pass: Find potential candidates
+        Map<Integer, Integer> freq = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        
         for (int num : nums) {
-            if (candidate1 != null && num == candidate1) {
-                count1++;
-            } else if (candidate2 != null && num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() > n / 3) {
+                result.add(entry.getKey());
             }
         }
-
-        // 2nd pass: Verify candidates
-        List<Integer> result = new ArrayList<>();
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (candidate1 != null && num == candidate1) count1++;
-            if (candidate2 != null && num == candidate2) count2++;
-        }
-
-        int n = nums.length;
-        if (count1 > n / 3) result.add(candidate1);
-        if (count2 > n / 3) result.add(candidate2);
 
         return result;
     }
